e3d28d1dfee33652e7d88c50550ab81876730523
Truong-kun <121338506+Truong-kun@users.noreply.github.com>
2023-09-10 22:06:59 +0700

    Add files via upload

---
+++ HW1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Text;
+
+namespace MyInfo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+
+            string fullname = "Nguyễn Văn Trường";
+            string studentID = "10122381";
+            string className = "12422TN";
+            string GitHub_username = "Truong-kun";
+            string email = "truongkun2k4@gmail.com";
+
+            Console.WriteLine($"Họ tên: {fullname}\tMã sinh viên: {studentID}\tLớp: {className}\tGitHub: {GitHub_username}\tEmail: {email}");
+        }
+    }
+}
