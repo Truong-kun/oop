922b80066baffb2dae3692c1fcce0d74a89f3e52
Truong-kun <121338506+Truong-kun@users.noreply.github.com>
2023-09-18 00:44:34 +0700

    Add files via upload

---
+++ HW2/Person.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW2
+{
+    internal class Person
+    {
+        private static List<Person> personList;
+        private static int top = 0;
+        string name, address;
+        int salary;
+        public string Name
+        {
+            get { return name; }
+            set {
+                if (value != null) name = value;
+                else name = "Unknown";
+            }
+        }
+        public string Address
+        {
+            get { return address; }
+            set { address = value; }
+        }
+        public int Salary
+        {
+            get { return salary; }
+            set { 
+                if (value >= 0) salary = value;
+            }
+        }
+        public Person() { }
+        public Person(string name, string address, int salary)
+        {
+            Name = name;
+            Address = address;
+            Salary = salary;
+        }
+        public void inputPersonInfo()
+        {
+            personList = new List<Person>();
+            Console.WriteLine("Enter infomation of person: ");
+            string name, address;
+            int salary;
+            while (true)
+            {
+                Console.Write(" - Name: ");
+                name = Console.ReadLine();
+                if (name != "") break;
+                Console.WriteLine("[!] Name mustn't be empty");
+            }
+            while (true)
+            {
+                Console.Write(" - Address: ");
+                address = Console.ReadLine();
+                if (address != "") break;
+                Console.WriteLine("[!] Name mustn't be empty");
+            }
+            while (true)
+            {
+                try
+                {
+                    Console.Write(" - Salary: ");
+                    salary = int.Parse(Console.ReadLine());
+                }
+                catch
+                {
+                    throw new Exception("[!] Salary must be a number");
+                }
+                if (salary > 0) break;
+            }
+            Name = name;
+            Address = address;
+            Salary = salary;
+        }
+        public void showPersonInfo()
+        {
+            Console.WriteLine("Information Entered: ");
+            Console.WriteLine("Name    : " + Name);
+            Console.WriteLine("Address : " + Address);
+            Console.WriteLine("Salary  : " + Salary);
+        }
+        public static List<Person> sortBySalary(List<Person> personList)
+        {
+            if (personList.Count == 0) throw new Exception("Can't sort an empty list");
+            else
+            {
+                for (int i = 0; i < personList.Count - 1; i++)
+                {
+                    for (int j = i + 1; j < personList.Count; j++)
+                    {
+                        if (personList[i].Salary > personList[j].Salary)
+                        {
+                            var t = personList[i];
+                            personList[i] = personList[j];
+                            personList[j] = t;
+                        }
+                    }
+                }
+
+                return personList;
+            }
+        }
+    }
+}
---
+++ HW2/Program.cs
@@ -0,0 +1,46 @@
+﻿using System.Text;
+
+namespace HW2
+{
+    internal class Program
+    {
+        static List<Person> personList = new List<Person>();
+        static void showPersonList()
+        {
+            Console.WriteLine("-----------------------------------------------------------------");
+            Console.WriteLine($"| {"Name",25} | {"Address",20} | {"Salary",10} |");
+            Console.WriteLine("-----------------------------------------------------------------");
+            foreach (var p in personList)
+            {
+                Console.WriteLine($"| {p.Name,25} | {p.Address,20} | {p.Salary,10} |");
+            }
+            Console.WriteLine("-----------------------------------------------------------------");
+            Console.WriteLine();
+        }
+        static void Main(string[] args)
+        {
+            Console.InputEncoding = Encoding.Unicode;
+            Console.OutputEncoding = Encoding.Unicode;
+
+            Console.Write("Nhập số người muốn nhập thông tin: ");
+            int n;
+            while (true)
+            {
+                string temp = Console.ReadLine();
+                if (int.TryParse(temp, out n)) break;
+                Console.WriteLine("[!] Số người phải lớn hơn 0");
+            }
+            for (int i = 0; i < n; i++)
+            {
+                var person = new Person();
+                person.inputPersonInfo();
+                personList.Add(person);
+            }
+            Console.WriteLine("\t\t\tPERSON LIST\n");
+            showPersonList();
+            personList = Person.sortBySalary(personList);
+            Console.WriteLine("\t\t\tSORTED PERSON LIST\n");
+            showPersonList();
+        }
+    }
+}
